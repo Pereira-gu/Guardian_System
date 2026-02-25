703aa1290b5777b0c6cfd5c9765aa84c4c5b7113
Gustavo Dos Santos Pereira <pereira.worker@gmail.com>
2026-02-25 19:35:45 -0300

    fix: bug ond os mesmo 10 processo aparecia repetidamente, arrumado, banco deletado e recriado, for loop arrumado

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import time
 from src import collector
 from src import engine, exporter
 
+pids_processados = set()
+
 
 def rodar_guardian():
     exporter.inicializar_db()
@@ -10,16 +12,20 @@ def rodar_guardian():
     while True:
         processos = collector.capturar_processos()
 
-        for proc in processos[:10]:
+        for proc in processos:
+            pid = proc["pid"]
             score = engine.calcular_risco(proc)
 
-            exporter.salvar_log(
-                modulo="Monitoramento de Processos",
-                descricao=f"Processo: {proc['name']} | PID: {proc['pid']}",
-                score_risco=score,
-            )
+            if pid not in pids_processados:
+                pids_processados.add(pid)
 
-            print(f"Monitorando: {proc['name']} | Risco: {score}% | Salvo No BD")
+                exporter.salvar_log(
+                    modulo="Monitoramento de Processos",
+                    descricao=f"Processo Suspeito: {proc['name']} | PID: {pid}",
+                    score_risco=score,
+                )
+                print(f"Monitorando: {proc['name']} | Risco: {score}% | Salvo No BD")
+
         print("-" * 30)
         time.sleep(60)
 
