c3689e484a5cc89184613e98320c318fb389b16a
Gustavo Dos Santos Pereira <pereira.worker@gmail.com>
2026-02-27 20:18:44 -0300

    feat: atualizei a função de calcular risco

--- src/engine.py
+++ src/engine.py
@@ -1,12 +1,23 @@
-def calcular_risco(evento):
+def calcular_risco(nome, cpu_usage=0, username="unknown"):
     score = 0
-    if "powershell" in evento["name"].lower():
-        score += 70
+    nome_low = nome.lower()
 
-    elif "chrome" in evento["name"].lower() or "code" in evento["name"].lower():
-        score += 5
+    processos_criticos = {
+        "powershell.exe": 60,
+        "cmd.exe": 40,
+        "regedit.exe": 50,
+        "vbc.exe": 70,
+    }
 
-    else:
+    score = processos_criticos.get(nome_low, 10)
+
+    if cpu_usage > 70:
         score += 20
+    elif cpu_usage > 30:
+        score += 10
 
-    return score
+    privilegiados = ["root", "admin", "system"]
+    if username.lower() in privilegiados:
+        score += 15
+
+    return max(0, min(score, 100))
