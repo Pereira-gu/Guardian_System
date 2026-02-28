a1d4e538b03457ab8363828305929396bdb996a2
Gustavo Dos Santos Pereira <pereira.worker@gmail.com>
2026-02-28 09:14:30 -0300

    adicinando no loop para capturar conexoes

--- main.py
+++ main.py
@@ -6,11 +6,11 @@ pids_processados = set()
 
 def rodar_guardian():
     exporter.inicializar_db()
+    pids_processados = set()
     print("Iniciando o Guardian...")
 
     while True:
         processos = collector.capturar_processos()
-
         for proc in processos:
             pid = proc["pid"]
             score = engine.calcular_risco(
@@ -21,10 +21,16 @@ def rodar_guardian():
 
             if pid not in pids_processados:
                 pids_processados.add(pid)
-
                 exporter.salvar_log("Processos", proc["name"], score)
                 print(f"Monitorando: {proc['name']} | Risco: {score}% | Salvo No BD")
 
+        conexoes = collector.capturar_conexoes()
+        for conn in conexoes:
+            exporter.salvar_log(
+                "Rede",
+                f"IP: {conn['ip_destino']}, Porta: {conn['porta']}, Status: {conn['status']}",
+                10,
+            )
         print("-" * 30)
         time.sleep(60)
 
